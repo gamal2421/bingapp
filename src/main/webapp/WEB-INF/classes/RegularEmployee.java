27714122a40c54f0006f839b1fc005367f318607
MohamedMando5 <mohamedemad22440@gmail.com>
2025-04-19 02:40:43 +0200

    Delete src/main/webapp/WEB-INF/classes directory

--- src/main/webapp/WEB-INF/classes/RegularEmployee.java
+++
@@ -1,8 +0,0 @@
-import java.util.Date;
-
-public class RegularEmployee extends User{
-    public RegularEmployee(int id, String firstName, String lastName, String gender, Date dob) {
-
-        super(id, firstName, lastName, gender, dob);
-    }
-}
