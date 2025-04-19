1a174bdd3c1381a4289b42b6fbd8c9a77308f99c
MohamedMando5 <mohamedemad22440@gmail.com>
2025-04-19 02:39:37 +0200

    Add files via upload

---
+++ src/main/webapp/WEB-INF/classes/Booking.java
@@ -0,0 +1,37 @@
+import java.util.Date;
+
+public class Booking {
+    private int bookingId;
+    private Date date;
+    private String type;
+
+    public Booking(int bookingId, Date date, String type) {
+        this.bookingId = bookingId;
+        this.date = date;
+        this.type = type;
+    }
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
---
+++ src/main/webapp/WEB-INF/classes/Main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Admin admin = new Admin(0 ,"ammar","nasser" , "male", new Date(2000, 8, 5));
+
+        RegularEmployee regularEmployee = new RegularEmployee(1 ,"Mohamed","Emad" , "male", new Date(2008, 5, 9));
+
+    }
+}
